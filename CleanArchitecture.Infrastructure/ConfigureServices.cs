3ff7d8883ad8f68736219dd841a0d4eb784c4348
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-08-24 10:38:04 +0300

    Initial commit

---
+++ CleanArchitecture.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services; 
+    }
+}
